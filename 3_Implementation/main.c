777652380fe1209699c6d6391500470ea9cd1c3d
Hrithik <hrithiksagar36@gmail.com>
2022-03-22 21:51:59 +0530

    main update

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,74 +1,15 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sudoku.h>
+
 #define N 9
-
-void print(int arr[N][N])
-{
-     for (int i = 0; i < N; i++)
-      {
-         for (int j = 0; j < N; j++)
-            printf("%d ",arr[i][j]);
-         printf("\n");
-       }
-}
-
-
-int isSafe(int grid[N][N], int row,
-                       int col, int num)
-{
-    for (int x = 0; x <= 8; x++)
-        if (grid[row][x] == num)
-            return 0;
-
-    for (int x = 0; x <= 8; x++)
-        if (grid[x][col] == num)
-            return 0;
-
-    int startRow = row - row % 3,
-                 startCol = col - col % 3;
-    for (int i = 0; i < 3; i++)
-        for (int j = 0; j < 3; j++)
-            if (grid[i + startRow][j +
-                          startCol] == num)
-                return 0;
-    return 1;
-}
- 
-
-
-
-
-int solveSudoku(int grid[N][N], int row, int col)
-{
-     
-    if (row == N - 1 && col == N)
-        return 1;
-    if (col == N)
-    {
-        row++;
-        col = 0;
-    }
-    if (grid[row][col] > 0)
-        return solveSudoku(grid, row, col + 1);
-    
-    for (int num = 1; num <= N; num++)
-    {
-        if (isSafe(grid, row, col, num)==1)
-        {
-            grid[row][col] = num;
-            if (solveSudoku(grid, row, col + 1)==1)
-                return 1;
-        }
-        grid[row][col] = 0;
-    }
-    return 0;
-}
- 
-
 
 int main()
 {
     // 0 means unassigned cells
+
+    printf("\nBelow is a sample 9X9 Sudoku Problem \n\n");
     int grid[N][N] = { { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
                        { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
                        { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
@@ -78,9 +19,63 @@ int main()
                        { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
                        { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
                        { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+
+
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            printf("%d  ", grid[i][j]);             
+        }
+        printf("\n");
+    }
+    
+    printf("\nBelow is the Solution\n\n");
     if (solveSudoku(grid, 0, 0)==1)
-        print(grid);
+    {
+        printSudoku(grid);
+    }
     else
-        printf("No solution exists");
-    return 0;
+    {
+        printf("No solution exists\n");
+    }
+
+printf("\nTo enter your Sudoku, enter 1. \n To exit,enter 2\n\n");
+
+int a, newArr[N][N];
+scanf("%d",&a);
+
+if(a==1)
+{
+   printf("\n\n -->ENTER BELOW YOUR OWN SUDOKU VALUES.<--\n NOTE: 1. Must be a 9X9 Sudoku.\n\t2. For empty spaces enter '0'.\n\t 3.The Gap while entering represents that value is being added to a new row.\n\n");
+
+            for(int i=0;i<9;i++){
+                for(int j=0;j<9;j++){
+                  printf("  ");
+                  scanf("%d",newArr[i][j]);
+                  printf("  ");
+                }
+            printf("\n");
+            }
+
+            printf("\nBelow is the Solution\n\n");
+            if (solveSudoku(newArr, 0, 0)==1)
+            {
+              printSudoku(newArr);
+            }
+            else
+            {
+              printf("No solution exists\n");
+            }
+}            
+            
+   else if(a==2)
+   {
+       exit(1);           
+   }
+
+printf("\t\tTHANK YOU");
+
+return 0;
+
 }
