63181e6d6d52abf004f8cbbe2df370c16af664ff
Hrithik <hrithiksagar36@gmail.com>
2022-03-28 15:40:34 +0530

    update

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"sudoku.h"
-
 #define N 9
 
 int main()
@@ -17,7 +16,6 @@ int main()
                        { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
                        { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
                        { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
-
 
     for(int i=0;i<N;i++)
     {
@@ -38,36 +36,46 @@ int main()
         printf("No solution exists\n");
     }
 
-printf("\nTo enter your Sudoku, enter 1. \n To exit,enter 2\n\n");
 
-int a, newArr[N][N];
-scanf("%d",&a);
 
-if(a==1)
-{
-   printf("\n\n -->ENTER BELOW YOUR OWN SUDOKU VALUES.<--\n NOTE: 1. Must be a 9X9 Sudoku.\n\t2. For empty spaces enter '0'.\n\t 3.The Gap while entering represents that value is being added to a new row.\n\n");
-            for(int i=0;i<9;i++){
-                for(int j=0;j<9;j++){
-                  printf("  ");
-                  scanf("%d",newArr[i][j]);
-                  printf("  ");
-                }
-            printf("\n");
-            }
-            printf("\nBelow is the Solution\n\n");
-            if (solveSudoku(newArr, 0, 0)==1)
-            {
-              printSudoku(newArr);
-            }
-            else
-            {
-              printf("No solution exists\n");
-            }
-}                    
-   else if(a==2)
-   {
-       exit(1);           
-   }
-printf("\t\tTHANK YOU");
+
+
+
+// printf("\nTo enter your Sudoku, enter 1. \n To exit,enter 2\n\n");
+// int a, *newArr[N][N];
+// scanf("%d",&a);
+// if(a==1)
+// {
+//    printf("\n\n -->ENTER BELOW YOUR OWN SUDOKU VALUES.<--\n NOTE: 1. Must be a 9X9 Sudoku.\n\t2. For empty spaces enter '0'.\n\t 3.The Gap while entering represents that value is being added to a new row.\n\n");
+
+//             for(int i=0;i<9;i++)
+//             {
+//                 for(int j=0;j<9;j++)
+//                 {
+//                   printf("  ");
+//                   scanf("%d",newArr[i][j]);
+//                   printf("  ");
+//                 }
+//             printf("\n");
+//             }
+
+//             printf("\nBelow is the Solution\n\n");
+
+//             if (solveSudoku(newArr[N][N], 0, 0)==1)
+//             {
+//               printSudoku(newArr[N][N]);
+//             }
+//             else
+//             {
+//               printf("No solution exists\n");
+//             }
+// }
+
+// else if(a==2)
+// {
+//     exit(1);           
+// }
+
+printf("\t\t THANK YOU");
 return 0;
 }
